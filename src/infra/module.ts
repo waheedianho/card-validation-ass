c96a5829e9cba65988ea1379ef67eabfb47be80f
Waheed Safiu <waheedianho65@gmail.com>
2026-09-03 15:06:45 +0100

    remove dataase support

--- src/infra/module.ts
+++ src/infra/module.ts
@@ -3,7 +3,7 @@ import { PostgresDatabaseModule } from './postgres/module.js';
 import { SecretsModule } from './secret/module.js';
 
 @Module({
-  imports: [PostgresDatabaseModule, SecretsModule],
-  exports: [PostgresDatabaseModule, SecretsModule],
+  imports: [SecretsModule],
+  exports: [SecretsModule],
 })
 export class InfraModule { }
